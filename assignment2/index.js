cc1948b5e6b8972d1400ff1e695f2a8032751911
vsasanka <62672912+vsasanka@users.noreply.github.com>
2022-09-03 23:15:29 -0700

    Add files via upload

---
+++ assignment2/index.js
@@ -0,0 +1,87 @@
+const httpServer = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const replaceTemplate = require('./modules/replaceTemplate');
+const dataImported = require('./data/data.json');
+
+
+/// Read data from file
+// Template
+const tempCourse = fs.readFileSync(
+    `${__dirname}/data/data.json`,
+    'utf-8'
+ );
+
+ /////////////////////////////////
+// Template
+const templateHTMLCourse = fs.readFileSync(
+    `${__dirname}/template/templateCourse.html`,
+    'utf-8'
+  );
+
+// //   function replaceTemplate(htmlStr, course){
+// const replaceTemplate = (htmlStr, course)=>{ // fat arrow function or lambda
+//     let output = htmlStr.replace(/{%NAME%}/g, course.courseName);
+//     output = output.replace(/{%IMAGE%}/g, course.image);
+//     output = output.replace(/{%FROM%}/g, course.from);
+//     output = output.replace(/{%INSTRUCTOR%}/g, course.instructor);
+//     output = output.replace(/{%CREDITS%}/g, course.credits);
+//     output = output.replace(/{%DESCRIPTION%}/g, course.description);
+//     output = output.replace(/{%ID%}/g, course.id);
+//     return output;
+// }
+
+
+
+ const dataObj = JSON.parse(tempCourse);// string to JavaScript Object JSON
+
+ /*
+ PV = (PMT/i)(1 - 1/(1+i)^n)
+ */
+
+ dataObj.forEach( (element) => {
+    console.log(element);
+    let pmt = Number(element.loanAmount);
+    let n = Number(element.loanTermYears)*12;
+    let i = Number(element.interest)/(100*12);
+    element.computedField = (pmt/i)*(1 - 1/(1+i)**n);
+});
+
+////////////////////////////////
+//Create Server
+// const server = httpServer.createServer(function (req, res) {// call back function
+const server = httpServer.createServer( (req, res) =>{// call back function
+
+    // const urlParameter = url.parse(req.url, true);
+    // console.log(JSON.stringify(urlParameter.query));// convert to String
+    // console.log(JSON.stringify(urlParameter.pathname));// convert to String
+    const {query,pathname} = url.parse(req.url, true); // object distructors
+    if(query.id){// if there is query parameter named id read as string
+        // Courses page
+        if (pathname === '/' || pathname.toLowerCase() === '/courses') {
+            res.writeHead(200, {// Every thing ran successfully
+                'Content-type': 'text/html'
+            });
+            const course = dataObj[Number(query.id)];// convert string to numeric value
+            const strCourseName = JSON.stringify(course);
+            const courseHTML = replaceTemplate(templateHTMLCourse, course);// function that will replace the course values in the HTML
+            //   res.end(` We received our first request from the client at resource ${urlParameter.pathname.toLowerCase()} with query parameter ${urlParameter.query.id}
+            //   ${JSON.stringify(course)}// convert object back to string
+            //   `)
+            res.end(courseHTML);
+        }
+    }
+    else{
+            res.writeHead(404, {// Server did not find what you were looking for
+                'Content-type': 'text/html'
+            });
+            res.end(`resource not found`)
+        }
+    });
+
+//Start Listening to requests
+server.listen(8000, 'localhost', ()=> {
+    console.log('Listening to requests on port 8000');
+});
+
