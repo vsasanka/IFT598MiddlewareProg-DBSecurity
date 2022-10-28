f5827f52d27b21e656cc738e40e7a311160c2910
vsasanka <62672912+vsasanka@users.noreply.github.com>
2022-10-27 22:39:42 -0700

    Add files via upload

---
+++ Module 5-Assignment 1 - JWT/server.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'),
+  User = require('./api/models/userModel'),
+  bodyParser = require('body-parser'),
+  jsonwebtoken = require("jsonwebtoken");
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/Tododb');
+// replaced connecting to the localhost with connection to mongodb
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+// picks values from the config.env to connect to DB
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    //useCreateIndex: true,
+    //useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+      if (err) req.user = undefined;
+      req.user = decode;
+      next();
+    });
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' })
+});
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
+
+module.exports = app;
