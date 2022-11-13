b9718789494f9cfc316ef2fed8ed71d4a04327f3
vsasanka <62672912+vsasanka@users.noreply.github.com>
2022-11-12 20:27:25 -0700

    Add files via upload

---
+++ M7A1 - server-side rendering with PUG server/controllers/viewsController.js
@@ -0,0 +1,39 @@
+
+
+exports.getLandingPAge = async (req, res) => {
+  res.status(200).render('overview', {
+    title: `Over View`
+  
+  });
+};
+
+exports.getCourse = async (req, res) => {
+  res.status(200).render('Course', {
+    title: `Get Course`
+  });
+};
+
+exports.createNewCourse = async (req, res) => {
+  res.status(200).render('newCourse', {
+    title: `Create New Course`
+  });
+};
+
+exports.getSignInForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Sign in New User'
+  });
+};
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
+
+exports.getLoginUser = (req, res) => {
+  const {email, password} = req.body;
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
+
